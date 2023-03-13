12bf5d3afa7c2282ebf0fb7e494f0e0149572a77
Yoonsik-Shin <sys25800994@gmail.com>
2023-03-13 11:00:57 +0900

    ssr 제외

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -6,7 +6,7 @@ export default function BoardsPage() {
     return <div>게시판/동적페이지{router.query.boardId}</div>;
 }
 
-export const getServerSideProps = () => {
-    // 서버사이드 렌더링시 out 폴더 생성 불가
-    // next.config.js에서 exportPathMap으로 제외시키기
-};
+// export const getServerSideProps = () => {
+//     // 서버사이드 렌더링시 out 폴더 생성 불가
+//     // next.config.js에서 exportPathMap으로 제외시키기
+// };
